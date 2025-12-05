d093f2314052a3a1222e8877b829b1207f1e7a4f
Jeremiah Nofrada <jnnofrada@gmail.com>
2025-12-04 23:25:38 -0500

    init commit

--- src/test/java/ParallelTests.java
+++ src/test/java/ParallelTests.java
@@ -1,19 +1,40 @@
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
 
+import java.nio.file.Paths;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ParallelTests {
+
+    static Playwright playwright;
+    static Browser browser;
+    static BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+
+        Browser.NewContextOptions contextOptions = new Browser.NewContextOptions()
+                .setRecordVideoDir(Paths.get("videos"));
+        context = browser.newContext(contextOptions);
+    }
+
+    @BeforeEach
+    void createPage() {
+        page = context.newPage();
+    }
 
     @Test
     @Order(1)
     void testGoogle() {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
             page.navigate("https://www.google.com");
             System.out.println("Title: " + page.title());
-            browser.close();
         }
     }
 
@@ -21,11 +42,21 @@ public class ParallelTests {
     @Order(2)
     void testBing() {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
             page.navigate("https://www.bing.com");
             System.out.println("Title: " + page.title());
-            browser.close();
         }
+    }
+
+
+    @AfterEach
+    void closePage() {
+        page.close();
+    }
+
+    @AfterAll
+    static void tearDown() {
+//        browser.close();
+        context.close();
+        playwright.close();
     }
 }
